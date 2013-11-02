168987cd7139cc791eb9ce5126f3de9429c5a584
Nathan Friend <contact@nathanfriend.com>
2013-11-02 14:14:01 -0500

    added arrow class

---
+++ Step.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpOverlay
+{
+    public class Step
+    {
+        private string _targetElementName;
+        public string TargetElementName
+        {
+            get { return _targetElementName; }
+            set { _targetElementName = value; }
+        }
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+    }
+}
