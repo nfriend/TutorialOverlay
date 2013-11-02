c6825dc22fb11a0e109cc5bb032ffbaa3e9799d0
Nathan Friend <contact@nathanfriend.com>
2013-11-01 22:57:34 -0500

    first commit
    Initial demo of how the overlay might be implemented.  Overlay "cutout"
    tracks with the mouse for demonstration purposes.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelpOverlay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            EllipseGeometry hole = this.Resources["Hole"] as EllipseGeometry;
+
+            if (hole != null)
+            {
+                hole.Center = e.GetPosition(this);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowSizeToRectConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace HelpOverlay
+{
+    public class WindowSizeToRectConverter : MarkupExtension, IMultiValueConverter
+    {
+        public WindowSizeToRectConverter() { }
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double windowActualHeight = Double.Parse(values[0].ToString());
+            double windowActualWidth = Double.Parse(values[1].ToString());
+            return new Rect(0, 0, windowActualWidth, windowActualHeight);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
